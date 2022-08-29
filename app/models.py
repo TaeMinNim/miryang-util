ccc0ba2150e733a894da1faf5745d1564f5920c1
TaeMin <xoals3094@naver.com>
2022-08-29 11:05:16 +0900

    modifiy

--- app/models.py
+++ app/models.py
@@ -16,3 +16,13 @@ class Order_Group(db.Model):
     delivery_cost = db.Column(db.Integer)
     account_number = db.Column(db.String(50))
     repUserId = db.Column(db.Integer, db.ForeignKey('USER.id', ondelete="CASCADE"))
+
+class Order_List(db.Model):
+    __tablename__="ORDER_LIST"
+    orderID = db.Column(db.Integer, primary_key=True)
+    groupID = db.Column(db.Integer, db.ForeignKey('ORDER_GROUP.groupID', ondelete="CASCADE"))
+    group = db.relationship('Order_Group', foreign_keys=[groupID], backref=db.backref('order_list'))
+    ordered_userID = db.Column(db.Integer,db.ForeignKey('USER.id', ondelete="CASCADE"))
+    menu = db.Column(db.String(50))
+    quantity = db.Column(db.Integer)
+    option = db.Column(db.String(50))
