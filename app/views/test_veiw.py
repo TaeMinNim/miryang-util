84aaac93f1c557f7b148d511b5df281d3a6a1e59
taemin <xoals3094@naver.com>
2022-09-15 11:40:52 +0900

    test

--- app/views/test_veiw.py
+++ app/views/test_veiw.py
@@ -1,13 +1,15 @@
-from flask import Blueprint, request, render_template
-
+from flask import Blueprint, request, redirect, url_for, render_template
 
 bp = Blueprint('test', __name__, url_prefix='/test')
 
-@bp.route('/signup/', methods=('POST','GET'))
-def signup():
+API_KEY = 'a3daa2c3fe72cd4cb9b0940afa4451e6'
+URL = 'http://13.209.94.41/test/login_kakao'
+
+@bp.route('/login_kakao/', methods=('POST','GET'))
+def loginKokao():
     if request.method == 'POST':
         json = request.get_json()
         print(json)
         return ('', 204)
     else:
-        return render_template('auth/test_signup_form.html')
+        return redirect('https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}'.format(REST_API_KEY=API_KEY, REDIRECT_URI=URL))
