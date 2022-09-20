10ce2a415407219800d78bdeb5f4cee9a1894544
taemin <xoals3094@naver.com>
2022-09-21 04:49:22 +0900

    username overlap check api

--- app/views/auth_view.py
+++ app/views/auth_view.py
@@ -8,14 +8,12 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/username-overlap-check/')
 def usernameOverlapCheck():
     username = request.args['user_name']
-    print(username)
-    db = pymysql.connect(host='localhost', port=3306, user='dbuser', passwd='!miryangUTIL2022',db='UTILITY_SERVICE', charset='utf8')
+    db = pymysql.connect(host='localhost', port=3306, user='root', passwd='1234',db='delivery', charset='utf8')
     cursor = db.cursor()
     sql = "SELECT id FROM SERVICE_USER WHERE username = '{username}'".format(username=username)
-    cursor.execute(sql)
-    userid = cursor.fetchall()
+    user = cursor.execute(sql)
     db.close()
-    if userid == None:
+    if user == 0:
         return jsonify(response=False)
     else:
         return jsonify(response=True)
