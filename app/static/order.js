37de2d70cb2ea180d676f76a5e72931ae074d6f4
taemin <xoals3094@naver.com>
2022-10-17 10:49:56 +0900

    url modify

--- app/static/order.js
+++
@@ -1,8 +0,0 @@
-const createGroupBtn = document.getElementById("create-group");
-const groupForm = document.getElementById("group-form");
-
-function showGroupForm(){
-    groupForm.classList.toggle("hidden");
-}
-
-createGroupBtn.addEventListener("click", showGroupForm);
