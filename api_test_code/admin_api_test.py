39268cc1b6f9212957150c6783a4cc730f9f44be
taemin <xoals3094@naver.com>
2022-10-19 02:01:02 +0900

    utility tool

---
+++ api_test_code/admin_api_test.py
@@ -0,0 +1,53 @@
+import requests
+
+aws_add_store = 'http://52.78.106.235:5000/admin/add-store'
+local_add_store = 'http://localhost:5000/admin/add-store'
+
+aws_add_menu = 'http://52.78.106.235:5000/admin/add-menu'
+local_add_menu = 'http://localhost:5000/admin/add-menu'
+
+
+aws_add_group = 'http://52.78.106.235:5000/admin/add-group'
+local_add_group = 'http://localhost:5000/admin/add-group'
+
+aws_add_option = 'http://52.78.106.235:5000/admin/add-option'
+local_add_option = 'http://localhost:5000/admin/add-option'
+
+
+store={
+    'store_name': '맘스터치',
+    'fee': 3000,
+    'min_order': 12000
+}
+
+menu={
+    'store_id': '6345a45f1c32cd7c4b64d895',
+    'section_name': '단품메뉴',
+    'menu_name': '싸이버거단품',
+    'menu_price': 50000
+}
+
+group={
+    'store_id': '6345a45f1c32cd7c4b64d895',
+    'section_name': '단품메뉴',
+    'menu_name': '싸이버거단품',
+    'group_name': '피클',
+    'min_orderable_quantity': 1,
+    'max_orderable_quantity': 1
+}
+
+option={
+    'store_id': '6345a45f1c32cd7c4b64d895',
+    'section_name': '단품메뉴',
+    'menu_name': '싸이버거단품',
+    'group_name': '피클',
+    'option_name': '기본',
+    'option_price': 0   
+}
+
+
+#requests.post(local_add_store, json=store)
+requests.post(local_add_menu,json=menu)
+requests.post(local_add_group,json=group)
+requests.post(local_add_option,json=option)
+
---
+++ api_test_code/order_api_test.py
@@ -0,0 +1,165 @@
+import requests
+import json
+import pprint
+aws_signup = 'http://52.78.106.235:5000/auth/signup'
+local_signup = 'http://localhost:5000/auth/signup'
+
+
+aws_login = 'http://52.78.106.235:5000/auth/login'
+local_login = 'http://localhost:5000/auth/login'
+
+
+aws_posting = 'http://52.78.106.235:5000/order/post/posting'
+
+local_post_detail = 'http://localhost:5000/order/post/detail/633d551c2f1c4e6d47ad3e35'
+
+
+aws_select = 'http://52.78.106.235:5000/order/'
+local_select = 'http://localhost:5000/order/section-menu-select?store_id=2'
+
+local_select_option = 'http://localhost:5000/order/group-option-select?menu_id=1664352908461'
+
+local_post_condition = 'http://localhost:5000/order/post/condition-switch'
+
+loginheader = {
+    'Authorization' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTY2Mzk4OTg4OTgyNiwibmlja19uYW1lIjoiYm9uZyJ9.FULK5UjhV7UnoRa8lUP7MrW0wccROJf9GUp7bac1tvo'
+}
+
+logindata = {
+    'user_name':'xoals3094',
+    'pw' : '~TMwhdkwhdk02'
+    
+}
+
+signupdata = {
+    'user_name':'testID',
+    'pw':'testPW',
+    'nick_name' : '테스트계정',
+    'student_num': '12345'
+}
+
+posting = {
+    'store_id': '6345a45f1c32cd7c4b64d895',
+    'title':'맘스터치 시키실분자운동',
+    'content': '같이 시켜요~',
+    'order_time': '2022-10-05 10:50',
+    'place': '기숙사',
+    'min_member' : 3,
+    'max_member' : 5
+    }
+
+update_posting = {
+    'post_id': '6345aa7b911ed10ac4cde368',
+    'title':'맘스터치 시키실분자운동',
+    'content': '같이 시켜요~',
+    'order_time': '2022-10-05 10:50',
+    'place': '기숙사',
+    'min_member' : 3,
+    'max_member' : 5
+    }
+post_condition = {
+    'post_id' : '6345aa7b911ed10ac4cde368'
+}
+
+order ={
+    'store_id': '6345a45f1c32cd7c4b64d895',
+    'post_id': '6345aa7b911ed10ac4cde368',
+    'orders': [
+        {
+            #불싸이버거 - 음료변경(제로콜라, 환타, 사이다) - 피클(기본)
+            'quantity': 5,
+            'menu_name': '불싸이버거',
+            'groups': [
+                {
+                    'group_id': 1665508590283,
+                    'options': [1665508592345, 1665508617919, 1665508624756]
+                },
+                {
+                    'group_id': 1665508644904,
+                    'options': [1665508646961]
+                }
+            ]
+        },
+        {
+            #딥치즈버거 - 피클(빼기)
+            'quantity': 3, 
+            'menu_name': '딥치즈버거',
+            'groups': [
+                {
+                    'group_id': 1665508715971,
+                    'options': [1665508718034]
+                }
+            ]
+        }
+    ]
+}
+
+update_order ={
+    'store_id': '6345a45f1c32cd7c4b64d895',
+    'post_id': '6345aa7b911ed10ac4cde368',
+    'orders': [
+        {
+            #불싸이버거 - 음료변경(제로콜라, 환타, 사이다) - 피클(기본)
+            'quantity': 3,
+            'menu_name': '불싸이버거',
+            'groups': [
+                {
+                    'group_id': 1665508590283,
+                    'options': [1665508592345]
+                },
+                {
+                    'group_id': 1665508644904,
+                    'options': [1665508646961]
+                }
+            ]
+        }
+    ]
+}
+#res = requests.post('http://52.78.106.235:5000/auth/login', json=logindata)
+
+#201
+#res = requests.get('http://localhost:5000/order/store/list', headers=loginheader)
+
+#202
+#res = requests.get('http://localhost:5000/order/menu/list?store_id=6345a45f1c32cd7c4b64d895', headers=loginheader)
+
+#203
+#res = requests.get('http://localhost:5000/order/menu/detail?store_id=6345a45f1c32cd7c4b64d895&menu_name=딥치즈버거', headers=loginheader)
+
+#204
+#res = requests.post('http://localhost:5000/order/post/posting', headers=loginheader, json=posting)
+
+#205
+res = requests.get('http://localhost:5000/order/post/detail/634e8af7b829facb9e6239e8', headers=loginheader)
+
+#206
+#GET
+#res = requests.get('http://localhost:5000/order/post/update?post_id=6345aa7b911ed10ac4cde368', headers=loginheader)
+#POST
+#res = requests.patch('http://localhost:5000/order/post/update', headers=loginheader, json=update_posting)
+
+#207
+#res = requests.patch('http://localhost:5000/order/post/condition-switch', headers=loginheader, json=post_condition)
+
+#209
+#res = requests.post('http://localhost:5000/order/ordering', headers=loginheader, json=order)
+
+#210
+#GET
+#res = requests.get('http://localhost:5000/order/ordering/update?post_id=6345aa7b911ed10ac4cde368', headers=loginheader)
+#PATCH
+#res = requests.patch('http://localhost:5000/order/ordering/update', headers=loginheader, json=update_order)
+
+#res = requests.post(local_signup, json=signupdata)
+#res = requests.post(local_login, json=logindata)
+#res = requests.post(local_posting, headers=loginheader, json=posting)
+#res = requests.get(local_select_option, headers=loginheader)
+
+#res = requests.get('http://localhost:5000/order/post/join/condition-switch?post_id=634e8af7b829facb9e6239e8', headers=loginheader)
+#res = requests.get('http://localhost:5000/order/post/list', headers=loginheader)
+
+#res = requests.get('http://52.78.106.235:5000/order/menu/detail?store_id=6345a45f1c32cd7c4b64d895&menu_name=딥치즈버거', headers=loginheader)
+
+
+
+print(res.text)
