32408a36f1670fbf639b1fbb312558ba8a0de5af
TaeMin <xoals3094@naver.com>
2022-11-02 00:12:17 +0900

    delete main_view

--- app/__init__.py
+++ app/__init__.py
@@ -13,8 +13,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_envvar('APP_CONFIG_FILE')
 
-    from app.views import auth_view, main_view, order_view, admin_veiw, taxi_view
-    app.register_blueprint(main_view.bp)
+    from app.views import auth_view, order_view, admin_veiw, taxi_view
     app.register_blueprint(auth_view.bp)
     app.register_blueprint(order_view.bp)
     app.register_blueprint(admin_veiw.bp)
