4f9e8dc8ea90bde2846aab1973dd6305df162772
TaeMin <xoals3094@naver.com>
2022-11-02 02:07:09 +0900

    admin api

--- api_test_code/admin_api_test.py
+++ api_test_code/admin_api_test.py
@@ -1,17 +1,17 @@
 import requests
 
 aws_add_store = 'http://52.78.106.235:5000/admin/delivery/add/store'
-local_add_store = 'http://localhost:5000/admin/add-store'
+local_add_store = 'http://localhost:5000/admin/delivery/add/store'
 
-aws_add_menu = 'http://52.78.106.235:5000/admin/add-menu'
-local_add_menu = 'http://localhost:5000/admin/add-menu'
+aws_add_menu = 'http://52.78.106.235:5000/admin/delivery/add/menu'
+local_add_menu = 'http://localhost:5000/admin/delivery/add/menu'
 
 
-aws_add_group = 'http://52.78.106.235:5000/admin/add-group'
-local_add_group = 'http://localhost:5000/admin/add-group'
+aws_add_group = 'http://52.78.106.235:5000/admin/delivery/add/group'
+local_add_group = 'http://localhost:5000/admin/delivery/add/group'
 
-aws_add_option = 'http://52.78.106.235:5000/admin/add-option'
-local_add_option = 'http://localhost:5000/admin/add-option'
+aws_add_option = 'http://52.78.106.235:5000/admin/delivery/add/option'
+local_add_option = 'http://localhost:5000/admin/delivery/add/option'
 
 
 store={
@@ -21,37 +21,49 @@ store={
 }
 
 menu={
-    'store_id': '6356cee8b760499670114a7f',
+    'store_id': '636151232f6ecd0a57ecfa1f',
     'section_name': '단품메뉴',
     'menu_name': '싸이버거단품',
     'menu_price': 50000
 }
 
 group={
-    'store_id': '6356cee8b760499670114a7f',
+    'store_id': '636151232f6ecd0a57ecfa1f',
     'section_name': '단품메뉴',
     'menu_name': '싸이버거단품',
-    'group_name': '피클',
-    'min_orderable_quantity': 1,
-    'max_orderable_quantity': 1
+    'groups': [
+        {
+            'group_name': '피클',
+            'min_orderable_quantity': 1,
+            'max_orderable_quantity': 1
+        }
+    ]
 }
 
 option={
-    'store_id': '6356cee8b760499670114a7f',
+    'store_id': '636151232f6ecd0a57ecfa1f',
     'section_name': '단품메뉴',
     'menu_name': '싸이버거단품',
-    'group_name': '피클',
-    'option_name': '빼기',
-    'option_price': 50000000   
+    'group_id': '0bc6296b-c8b5-4ed1-92df-016f2c7e276c',
+    'options':[
+        {
+            'option_name': '빼기',
+            'option_price': 50000000
+        },
+        {
+            'option_name': '기본',
+            'option_price': 0
+        }
+     ]       
 }
 
 platform = { 'platform_name' : '기숙사' }
 
 
-requests.post(aws_add_store, json=store)
-#requests.post(aws_add_menu,json=menu)
-#requests.post(aws_add_group,json=group)
-#requests.post(aws_add_option,json=option)
+#res = requests.post(aws_add_store, json=store)
+#res = requests.post(aws_add_menu,json=menu)
+#res = requests.post(aws_add_group,json=group)
+res = requests.post(aws_add_option,json=option)
 
 #requests.post('http://localhost:5000/admin/taxi/platform',json=platform)
-
+print(res.text)
